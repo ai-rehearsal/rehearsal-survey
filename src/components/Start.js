4c059d6b7b524ab154c14981b2347d0ac36a7ed0
yangjun-ux <nov16jun@gmail.com>
2020-09-08 10:56:29 +0900

    add Buttons

--- src/components/Start.js
+++ src/components/Start.js
@@ -8,8 +8,10 @@ const Background = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
+    flex-direction: column;
     justify-content: center;
-    align-items: center;
+    align-items: flex-start;
+    padding-left: 70px;
 `;
 
 const TextWrap = styled.div`
@@ -18,6 +20,19 @@ const TextWrap = styled.div`
     font-weight: 500;
     line-height: 36pt;
     margin-bottom: 75px;
+    @media (max-width: 500px) {
+        font-size: 22pt;
+        line-height: 32pt;
+    }
+`;
+
+const Button1 = styled.div`
+    color: white;
+    border-bottom: 4px solid rgb(251, 188, 6);
+    font-size: 20pt;
+    font-weight: 500;
+    line-height: 36pt;
+    margin-top: 400px;
     @media (max-width: 500px) {
         font-size: 22pt;
         line-height: 32pt;
@@ -34,10 +49,13 @@ class Start extends React.Component {
         return (
             <FadeInUpDiv>
                 <Background>
-                        <TextWrap>
+                    <TextWrap>
                             당신에게 맞는<br/>
                             <span style={{color: "rgb(251, 188, 6)", fontWeight: 900}}>직무를 추천</span>해 드립니다.
-                        </TextWrap>
+                    </TextWrap>
+                    <Button1>
+                        테스트 시작하기 >
+                    </Button1>
                 </Background>
             </FadeInUpDiv>
         );
